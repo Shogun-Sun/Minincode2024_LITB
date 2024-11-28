1a691ce288a4a74c779d9752d34c77078c0c81e1
Shogun_Sun <fima8516@gmail.com>
2024-11-29 01:50:51 +0300

    map fixed

--- public/js/map.js
+++ public/js/map.js
@@ -1,8 +1,16 @@
-ymaps.ready(function () {
-    var myMap = new ymaps.Map('map', {
-            center: [55.751574, 37.573856],
-            zoom: 9
-        }, {
-            searchControlProvider: 'yandex#search'
-        })
-});
+ymaps.ready(init);
+
+function init() {
+    var map = new ymaps.Map("map", {
+        center: [55.751574, 37.573856], // Координаты центра карты (Москва, например)
+        zoom: 10 // Уровень приближения
+    });
+
+    // Пример добавления метки
+    var placemark = new ymaps.Placemark([55.751574, 37.573856], {
+        hintContent: 'Это Москва!',
+        balloonContent: 'Столица России'
+    });
+
+    map.geoObjects.add(placemark);
+}
