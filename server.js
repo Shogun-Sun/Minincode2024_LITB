0c79cd4713874d66ec55c5b7c725b88cfb4ae954
Shogun_Sun <fima8516@gmail.com>
2024-11-28 14:04:41 +0300

    update database config

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const path = require("path"); 
 const app = express();
 const router = require("./public/pageRouter");
+const {sequelize, User} = require("./server/database");
 
 app.use(express.static(path.join(__dirname, "public")));
 app.use(router);
--- server/database.js
+++ server/database.js
@@ -1,48 +1,108 @@
-const {Sequelize, DataTypes} = require("sequelize");
-const sequelize = new Sequelize("postgres://minincode:minincode@193.233.114.248:5434/minincode");
+const { Sequelize, DataTypes, STRING } = require("sequelize");
 
+// Подключение к базе данных PostgreSQL
+const sequelize = new Sequelize(
+  "postgres://minincode:minincode@193.233.114.248:5434/minincode"
+);
 
+// Определение модели User
 const User = sequelize.define(
-    'User',
-    {
-        id: {
-            type: DataTypes.INTEGER,
-            primaryKey: true,
-            autoIncrement: true,
-        },
-        surname: {
-            type: DataTypes.STRING(100),
-            allowNull: false,
-        },
-        name: {
-            type: DataTypes.STRING(100),
-            allowNull: false,
-        },
-        middle_name: {
-            type: DataTypes.STRING(100),
-            allowNull: true, 
-        },
-        email: {
-            type: DataTypes.STRING(100),
-            allowNull: false,
-            unique: true,
-            validate: {
-                isEmail: true, 
-            },
-        },
-        password: {
-            type: DataTypes.STRING(255), 
-            allowNull: false, 
-        },
+  "User",
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    surname: {
+      type: DataTypes.STRING(100),
+      allowNull: false,
+    },
+    name: {
+      type: DataTypes.STRING(100),
+      allowNull: false,
+    },
+    middle_name: {
+      type: DataTypes.STRING(100),
+      allowNull: true,
+    },
+    email: {
+      type: DataTypes.STRING(100),
+      allowNull: false,
+      unique: true,
+      validate: {
+        isEmail: true,
+      },
+    },
+    password: {
+      type: DataTypes.STRING(255),
+      allowNull: false,
+    },
+    role: {
+      type: DataTypes.STRING(100),
+      allowNull: false,
+      defaultValue: "user",
+      validate: {
+        isIn: [["guest", "user", "organization", "admin"]],
+      }
+    },
+  },
+  {
+    tableName: "users",
+    timestamps: false,
+  }
+);
+
+//Определении модели Организаций
+const Organization = sequelize.define(
+  "Organization",
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    ogrn: {
+      type: DataTypes.STRING(24),
+      allowNull: false,
+      unique: true,
+    },
+    address: {
+      type: DataTypes.STRING(255),
+      allowNull: false,
+    },
+    mail: {
+      type: DataTypes.STRING(100),
+      allowNull: false,
+    },
+    phone: {
+      type: DataTypes.STRING(20),
+      allowNull: false,
+    },
+    organization_name: {
+      type: DataTypes.STRING(255),
+      allowNull: false,
+    },
+    password: {
+      type: DataTypes.STRING(255),
+      allowNull: false,
     }
+  },
+  {
+    tableName: "organizations",
+    timestamps: false,
+  }
 );
 
 sequelize
-    .authenticate()
-    .then(() => {
-        console.log("Подключение успешно");
-        return User.sync({alter: true});
-    })
-    .catch((error) => {
-        console.log(error);
-    })
+  .authenticate()
+  .then(() => {
+    console.log("Подключение успешно");
+    User.sync({alter: true});
+    Organization.sync({alter: true});
+  })
+  .catch((error) => {
+    console.log("Ошибка при подключении:", error);
+  });
+
+module.exports = { User, sequelize };
