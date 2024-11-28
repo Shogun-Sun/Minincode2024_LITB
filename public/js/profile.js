a3a0de10f0b327a84214beaf006553d8646a8876
makar-md <150604154+makar-md@users.noreply.github.com>
2024-11-29 00:33:24 +0300

    add avatar

--- public/js/profile.js
+++ public/js/profile.js
@@ -9,6 +9,23 @@ document.addEventListener("DOMContentLoaded", () => {
     .then((data) => {
         if(data.status === "error"){
             console.log(data.avatar); 
+            const avatar = document.createElement("img");
+            const fileInput = document.createElement("input");
+            const avatar_container = document.createElement("label");
+            fileInput.type = "file";
+            fileInput.name = "avatar";
+            fileInput.id = "input_avatar";
+
+            avatar.src = data.avatar;
+            avatar.id = 'avatar';
+            avatar.alt = "Аватар пользователя";
+
+            avatar_container.setAttribute('for', 'input_avatar');
+            avatar_container.appendChild(avatar);
+            avatar_container.appendChild(fileInput);
+
+        // Аватар и текст приветствия
+        document.querySelector('main').appendChild(avatar_container);
         } else {
             console.log(data);
         }
