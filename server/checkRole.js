45017544b805ebcc17d0b9ea42950e35fb892dde
Shogun_Sun <fima8516@gmail.com>
2024-11-28 22:35:37 +0300

    access review

--- server/checkRole.js
+++ server/checkRole.js
@@ -1,10 +1,35 @@
-// const path = require("path");
-// function checkRole(req, res, next) {
-//     if (req.session.user && req.session.user.role === "admin") {
-//       return next();
-//     } else {
-//       return res.status(403).sendFile(path.join(__dirname, "../pages", "access_denied.html"));
+const path = require("path");
+
+function checkRole(req, res, next) {
+  if (req.session.user && req.session.user.role === "admin") {
+    return next();
+  }
+
+//   if (req.session.user && req.session.user.role === "user") {
+   
+//     const allowedPathsForUser = ["/user/profile", "/user/settings"];
+//     if (allowedPathsForUser.includes(req.originalUrl)) {
+//       return next();  // Доступ разрешен
 //     }
 //   }
 
-//   module.exports = {checkRole};
+  if (req.session.user && req.session.user.role === "organization") {
+    const allowedPathsForOrg = ["/organization/profile", "/organization/dashboard"]; 
+    if (allowedPathsForOrg.includes(req.originalUrl)) {
+      return next();  
+    }
+  }
+
+  // Для гостей: разрешаем доступ только к определенным страницам
+  if (!req.session.user) {
+    const allowedPathsForGuest = ["/", "/user/log/page", "/user/reg/page"];
+    if (allowedPathsForGuest.includes(req.originalUrl)) {
+      return next();  // Доступ разрешен
+    }
+  }
+
+  // Если путь не разрешен для текущей роли, показываем страницу отказа в доступе
+  res.status(403).sendFile(path.join(__dirname, "../pages", "permissiondenied.html"));
+}
+
+module.exports = { checkRole };
