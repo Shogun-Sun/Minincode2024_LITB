abbd3c57cd14737bfdb5dbd6f153d7924a9c8ac6
Denchik-red <denaftogen19@gmail.com>
2024-11-28 19:40:53 +0300

    add moderation

--- public/js/moderationorg.js
+++ public/js/moderationorg.js
@@ -5,12 +5,66 @@ fetch("/organization/get/data", {
     },
 })
   .then(res => res.json())
-  .thent(data => {
+  .then(data => {
     console.log(data);
-    let tr = document.createElement("tr");
-    let orgName = document.createElement("td");
-    let agree = document.createElement("td");
-    let accept = document.createElement("button");
-    let reject = document.createElement("button");
-    //orgName.innerText = data.name;
+    data.data.forEach((org) => {
+      /* объявление переменных */
+      let tr = document.createElement("tr");
+      let OGRN = document.createElement("td");
+      let orgName = document.createElement("td");
+      let agree = document.createElement("td");
+      let accept = document.createElement("button");
+      let reject = document.createElement("button");
+      /* /объявление переменных */
+      /* заполнение атрибутов */
+      OGRN.innerText = org.ogrn;
+      orgName.innerText = org.organization_name;
+      accept.innerText = "Одобрить";
+      reject.innerText = "Отклонить";
+      OGRN.setAttribute("data-bs-toggle", "modal")
+      OGRN.setAttribute("data-bs-target", "#exampleModal")
+      orgName.setAttribute("data-bs-toggle", "modal")
+      orgName.setAttribute("data-bs-target", "#exampleModal")
+      accept.classList.add("accept")
+      reject.classList.add("reject")
+
+      /* /заполнение атрибутов */
+      tr.onclick = () => {
+        document.querySelector(".modal-body").innerText = 
+        `ОГРН: ${org.ogrn}
+        Назавание организации: ${org.organization_name}
+        Электронная почта: ${org.email}
+        Телефон: ${org.phone}
+        Адрес: ${org.address}
+        `;
+        document.querySelector("#accept").onclick = () => {
+          accept.click();
+        }
+      }
+      agree.append(accept, reject);
+      agree.onclick = (elem) => {
+        if (elem.target.classList.contains("accept")) {
+          console.log({id: org.id, status: 'save', organization: org})
+        // fetch("", {
+        //   method: "POST",
+        //   headers: {
+        //       "Content-Type": "application/json",
+        //   },
+        //   body: JSON.stringify({status: "save", organization: org})
+        // })
+        } else if (elem.target.classList.contains("reject")) {
+          console.log({id:org.id, status: 'delete'})
+          // fetch("", {
+          //   method: "POST",
+          //   headers: {
+          //       "Content-Type": "application/json",
+          //   },
+          //   body: JSON.stringify({status: 'delete'})
+          // })
+        }
+        tr.remove();
+      }
+      tr.append(OGRN, orgName, agree);
+      document.querySelector("tbody").appendChild(tr);
+    })
   })
