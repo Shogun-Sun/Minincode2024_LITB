b8c5b3eb35daba37790183fe4643d349a914fba1
Shogun_Sun <fima8516@gmail.com>
2024-11-28 21:06:48 +0300

    fix bugs

--- public/js/index.js
+++ public/js/index.js
@@ -1,9 +1,10 @@
-const home = document.getElementById('section_1');
-log_in.addEventListener('click', ()=>{
-    window.location.href = '/';
-})
+    const home = document.getElementById('section_1');
+    const log_in = document.getElementById('section_2');
 
-const log_in = document.getElementById('section_2');
-log_in.addEventListener('click', ()=>{
-    window.location.href = "/user/reg/page";
-})
+    home.addEventListener('click', () => {
+        window.location.href = '/';
+    });
+
+    log_in.addEventListener('click', () => {
+        window.location.href = "/user/reg/page";
+    });
