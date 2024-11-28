2433c3290e0ca6a88ab5efbde60625d1c48b1b20
Shogun_Sun <fima8516@gmail.com>
2024-11-28 11:51:14 +0300

    refresh root project folder

--- server.js
+++ server.js
@@ -1,12 +1,11 @@
 const express = require("express");
+const path = require("path"); 
 const app = express();
-const router = require("./server_modules/pageRouter");
-const path = require("path");
+const router = require("./public/pageRouter");
 
 app.use(express.static(path.join(__dirname, "public")));
 app.use(router);
 
- 
 app.listen(3000, () => {
     console.log("Server запущен");
-})
+});
