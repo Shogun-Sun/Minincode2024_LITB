95af512871987865c0c4608190d463b3dd64c549
makar-md <150604154+makar-md@users.noreply.github.com>
2024-11-28 15:01:01 +0300

    add href

---
+++ public/js/index.js
@@ -0,0 +1,9 @@
+const home = document.getElementById('section_1');
+log_in.addEventListener('click', ()=>{
+    window.location.href = '/';
+})
+
+const log_in = document.getElementById('section_2');
+log_in.addEventListener('click', ()=>{
+    window.location.href = '/user/reg';
+})
